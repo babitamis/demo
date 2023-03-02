eda470b32e4cd892378a028d86baa599d49540e3
apple <apple@example.com>
2023-03-01 23:28:21 -0600

    yo

--- test.java
+++ test.java
@@ -1,2 +1,2 @@
 
-hi i am taking devops training and m learning every day .i have added new file today again AND I WILL COMMIT NOW .
+hi i am taking devops training and m learning every day .i have added new file today again AND I WILL COMMIT NOW .commitimg to azure devops
